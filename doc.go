bba264a544629d23edaa208b4eecbe9094dd194e
TangRufus <tangrufus@gmail.com>
2025-10-26 11:44:08 +0000

    Doc: Mention `composer-semver`

--- doc.go
+++ doc.go
@@ -1,6 +1,13 @@
 /*
 Package comver provides the ability to work with [composer supported versions] in Go.
 
+Due to implementation complexity, this package only supports a subset of composer versioning.
+Refer to the [version_test.go] for examples.
+
+For better compatibility, use [composer-semver] instead.
+
 [composer supported versions]: https://github.com/composer/semver/
+[version_test.go]: https://github.com/typisttech/comver/blob/main/version_test.go
+[composer-semver]: https://github.com/typisttech/composer-semver/
 */
 package comver
